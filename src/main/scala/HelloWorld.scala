e1ec91216d997852c7ac1010943637a54c624319
alina <alinashevchuk27@gmail.com>
2019-06-02 17:19:49 +0300

    Added new modified lines plus added with computation for HelloWorld.scala inside feature1 branch

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -3,5 +3,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World from HelloWorld.scala !")
 	println("Hello, from Anabel !")
+    println("Hello, out of HelloWorld.scala !")
+    println("Please, count: " + (1 + 1))
   }
 }
